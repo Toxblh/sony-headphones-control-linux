f616f6f3dbb0ee469160096ac96299e9e78eea88
Anton Palgunov <toxblh@gmail.com>
2020-10-17 02:57:00 +0100

    upd

--- prototype/main.py
+++ prototype/main.py
@@ -1,6 +1,19 @@
 # Thanks https://github.com/impankratov/sony-headphones-control-py
+import enum
 import sys
+import dbus
 import bluetooth
+import json
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QStringListModel
+
+
+class Mode(enum.Enum):
+    NoiseCancelling = 'noise-cancelling'
+    WindCancelling = 'wind-cancelling'
+    AmbientSound = 'ambient-sound'
+    Disabled = 'disable'
 
 # enabled - any NC/WC/AS modes
 # noiseCancelling - 0, 1, 2 modes of NC
@@ -22,62 +35,180 @@ def getPacket(enabled: bool, noiseCancelling: int, volume: int, voice: bool):
 
     return bytes(readyPacket)
 
-addr = None
-mode = None
 
-if len(sys.argv) < 3:
-    print("Invalid arguments, please specify BT address and desired mode\n\nChoose one of mode:\n  noise-cancelling\n  wind-cancelling\n  ambient-sound\n  disable")
-    sys.exit(0)
-else:
-    addr = sys.argv[1]
-    mode = sys.argv[2]
+def setMode(mode):
+
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    print('config', config['device'])
+
+    addr = config['device']
+    print("Set mode", mode)
     print("Searching for {}...".format(addr))
 
-uuid = "96cc203e-5068-46ad-b32d-e316f5e069ba"
+    uuid = "96cc203e-5068-46ad-b32d-e316f5e069ba"
 
-service_matches = bluetooth.find_service(uuid=uuid, address=addr)
+    service_matches = bluetooth.find_service(uuid=uuid, address=addr)
 
-print('service_matches', service_matches)
+    print('service_matches', service_matches)
 
-if len(service_matches) == 0:
-    print("Couldn't find the device service.")
-    sys.exit(0)
+    if len(service_matches) == 0:
+        print("Couldn't find the device service.")
+        sys.exit(0)
 
-first_match = service_matches[0]
-print('first_match', first_match)
-port = first_match["port"]
-host = first_match["host"]
+    first_match = service_matches[0]
+    print('first_match', first_match)
+    port = first_match["port"]
+    host = first_match["host"]
 
-ambientSoundBytes = None
+    ambientSoundBytes = None
 
-if mode == 'noise-cancelling':
-    # Noise cancelling
-    ambientSoundBytes = getPacket(True, 2, 0, False)
+    if mode == Mode.NoiseCancelling:
+        # Noise cancelling
+        ambientSoundBytes = getPacket(True, 2, 0, False)
 
-elif mode == 'wind-cancelling':
-    # Wind cancelling
-    ambientSoundBytes = getPacket(True, 1, 0, False)
+    elif mode == Mode.WindCancelling:
+        # Wind cancelling
+        ambientSoundBytes = getPacket(True, 1, 0, False)
 
-elif mode == 'ambient-sound':
-    # Ambient sound
-    ambientSoundBytes = getPacket(True, 0, 19, False)
+    elif mode == Mode.AmbientSound:
+        # Ambient sound
+        ambientSoundBytes = getPacket(True, 0, 19, False)
 
-elif mode == 'disable':
-    # Disabled ambient sound
-    ambientSoundBytes = getPacket(False, 0, 0, False)
+    elif mode == Mode.Disabled:
+        # Disabled ambient sound
+        ambientSoundBytes = getPacket(False, 0, 0, False)
 
-else:
-    print('Unknown mode, exiting')
-    sys.exit(0)
+    else:
+        print('Unknown mode, exiting')
+        sys.exit(0)
 
-# print('ambientSoundBytes', ambientSoundBytes)
+    # print('ambientSoundBytes', ambientSoundBytes)
 
-print("Connecting to {} to enable {}".format(host, mode))
+    print("Connecting to {} to enable {}".format(host, mode))
 
-# Create the client socket
-sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-print('host', host)
-print('port', port)
-sock.connect((host, port))
-sock.send(ambientSoundBytes)
-sock.close()
+    # Create the client socket
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    print('host', host)
+    print('port', port)
+    sock.connect((host, port))
+    sock.send(ambientSoundBytes)
+    sock.close()
+
+
+def getBlueDevices():
+    bus = dbus.SystemBus()
+    obj = bus.get_object('org.bluez', "/")
+    manager = dbus.Interface(obj, "org.freedesktop.DBus.ObjectManager")
+    objects = manager.GetManagedObjects()
+    devices = []
+    for path in objects.keys():
+        interfaces = objects[path]
+        for interface in interfaces.keys():
+            if interface == "org.bluez.Device1":
+                devices.append(path)
+
+    bt_devices = []
+    for device in devices:
+        obj = bus.get_object('org.bluez', device)
+        info = dbus.Interface(obj, 'org.freedesktop.DBus.Properties')
+        bt_devices.append({
+            "name": str(info.Get("org.bluez.Device1", "Name")),
+            "addr": str(info.Get("org.bluez.Device1", "Address"))
+        })
+
+    return bt_devices
+
+
+def showSettings():
+    print("Connecting to")
+
+
+def saveDevice(index):
+    device = view.model().itemData(index)
+    print(device[0])
+    for x in devices:
+        if x['name'] == device[0]:
+            devAddr = x['addr']
+
+    devLabel.setText(device[0] + ": " + devAddr)
+    config = {'device': devAddr, 'name': device[0]}
+
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
+
+
+app = QApplication([])
+app.setQuitOnLastWindowClosed(False)
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+# Tray
+icon = QIcon("icon.png")
+
+tray = QSystemTrayIcon()
+tray.setIcon(icon)
+tray.setVisible(True)
+
+act_NC = QAction('Noise cancelling')
+act_NC.triggered.connect(lambda: setMode(Mode.NoiseCancelling))
+
+act_WC = QAction('Wind cancelling')
+act_WC.triggered.connect(lambda: setMode(Mode.WindCancelling))
+
+act_AS = QAction('Ambient sound')
+act_AS.triggered.connect(lambda: setMode(Mode.AmbientSound))
+
+act_Dis = QAction('Disabled ambient sound')
+act_Dis.triggered.connect(lambda: setMode(Mode.Disabled))
+
+settings = QAction("Settings")
+settings.triggered.connect(showSettings)
+
+quit = QAction('Quit')
+quit.triggered.connect(app.quit)
+
+menu = QMenu()
+print(config['name'])
+a = QAction(config['name'])
+
+menu.addAction(a)
+menu.addSeparator()
+menu.addAction(act_NC)
+menu.addAction(act_WC)
+menu.addAction(act_AS)
+menu.addAction(act_Dis)
+menu.addSeparator()
+menu.addAction(settings)
+menu.addAction(quit)
+
+tray.setContextMenu(menu)
+
+# Window
+window = QWidget()
+window.setWindowTitle('Sony control')
+window.setGeometry(1500, 700, 400, 200)
+
+layout = QVBoxLayout()
+
+devices = getBlueDevices()
+options = []
+for device in devices:
+    options.append(device['name'])
+
+model = QStringListModel(options)
+view = QListView()
+view.setModel(model)
+view.clicked.connect(saveDevice)
+
+devLabel = QLabel("Current device: " +
+                  config['name'] + ": " + config['device'])
+
+layout.addWidget(devLabel)
+layout.addWidget(view)
+window.setLayout(layout)
+window.show()
+
+app.exec_()
